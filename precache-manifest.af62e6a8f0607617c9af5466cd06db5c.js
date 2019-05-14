3d5da0cb77156ac6dd1dab72189ebf4c159d5662
Rina Andriana <rina.andriana@knowit.no>
2019-05-14 21:40:25 +0200

    Updates

---
+++ precache-manifest.af62e6a8f0607617c9af5466cd06db5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4727a6dc5ed02ca2fe121e2507448a",
+    "url": "/blueways-website/index.html"
+  },
+  {
+    "revision": "230d07f52e31ae1c4159",
+    "url": "/blueways-website/static/css/main.de175070.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/blueways-website/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "230d07f52e31ae1c4159",
+    "url": "/blueways-website/static/js/main.74b6ba90.chunk.js"
+  },
+  {
+    "revision": "c16869e6f1ddc91d9d65",
+    "url": "/blueways-website/static/js/runtime~main.d19f9992.js"
+  },
+  {
+    "revision": "61535b08fa43ff02219c525936dfafd0",
+    "url": "/blueways-website/static/media/blueways_logo.61535b08.svg"
+  },
+  {
+    "revision": "b877e4165c576a49c7d08a6dfc90fea4",
+    "url": "/blueways-website/static/media/hero_image.b877e416.jpg"
+  }
+]);
