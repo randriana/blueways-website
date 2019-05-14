7b08b1eb55fe0fd368dd3a8c0e1c406deb447e59
Rina Andriana <rina.andriana@knowit.no>
2019-05-14 21:36:00 +0200

    Rename build to docs

---
+++ docs/precache-manifest.be4854448578b325916479b2cee6ffe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddce5a77dedce3f0a109bd0038f3d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114f45a3d2aff5c87378",
+    "url": "/static/css/main.de175070.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "114f45a3d2aff5c87378",
+    "url": "/static/js/main.12ba7fd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61535b08fa43ff02219c525936dfafd0",
+    "url": "/static/media/blueways_logo.61535b08.svg"
+  },
+  {
+    "revision": "b877e4165c576a49c7d08a6dfc90fea4",
+    "url": "/static/media/hero_image.b877e416.jpg"
+  }
+]);
